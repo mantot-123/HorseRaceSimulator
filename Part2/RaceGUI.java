57794407a3dd73556ff51b1e90ebfddc718282d1
emman123 <67055497+emman123667@users.noreply.github.com>
2025-04-10 13:26:19 +0100

    Initial commit: Added 3 files: Horse.java, Main.java and RaceGUI.java

---
+++ Part2/RaceGUI.java
@@ -0,0 +1,15 @@
+package Part2;
+
+import java.util.*;
+import javax.swing.*;
+
+public class RaceGUI {
+    private int raceLength;
+    private ArrayList<Horse> laneHorses;
+    private Horse winningHorse;
+    private JFrame frame;
+
+    RaceGUI(int raceLength) {
+        this.raceLength = raceLength;
+    }
+}
