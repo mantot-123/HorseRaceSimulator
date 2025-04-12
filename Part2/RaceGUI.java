960515a11630e493c3cfe51ab9dfebc0d6748f15
emman123 <67055497+emman123667@users.noreply.github.com>
2025-04-12 21:56:47 +0100

    Commit: Created method openRaceMenuWindow() to show a graphical menu to configure racetrack distance and add customisable participating horses to a race

--- Part2/RaceGUI.java
+++ Part2/RaceGUI.java
@@ -1,15 +1,68 @@
+import java.awt.*;
 import java.util.*;
 import javax.swing.*;
-import java.awt.*;
-import javax.swing.border.Border;
 
 public class RaceGUI {
+    private JFrame menuFrame = new JFrame("New race");
+    private JFrame raceFrame = new JFrame("Race in progress...");
+    private ArrayList<JTextField> horseNameFields = new ArrayList<JTextField>();
+    private ArrayList<JTextField> horseSymbolFields = new ArrayList<JTextField>();
+    int horseCount = 0;
+
     private int raceLength;
     private ArrayList<HorseV2> laneHorses = new ArrayList<HorseV2>();
     private HorseV2 winningHorse;
 
     public RaceGUI(int raceLength) {
-        this.raceLength = raceLength;
+        openRaceMenuWindow();
+    }
+
+    public void openRaceMenuWindow() {
+        menuFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        menuFrame.setSize(1000, 500);
+        menuFrame.setLayout(new BorderLayout());
+
+        JPanel panel1 = new JPanel(new GridLayout(0, 2, 10, 10));
+        JTextField raceLengthField = new JTextField(15);
+        JButton addHorseBtn = new JButton("Add horse ");
+
+        addHorseBtn.addActionListener(e -> {
+            if(horseCount >= 6) {
+                JOptionPane.showMessageDialog(null, "ERROR: You can only have up to 6 horses in the race");
+                return;
+            }
+
+            horseCount++;
+            JTextField horseNameField = new JTextField(15);
+            JTextField horseSymbolField = new JTextField(15);
+
+            horseNameFields.add(horseNameField);
+            horseSymbolFields.add(horseSymbolField);
+
+            panel1.add(new JLabel("Horse #" + horseCount + " name: "));
+            panel1.add(horseNameField);
+            panel1.add(new JLabel("Horse #" + horseCount + " symbol: "));
+            panel1.add(horseSymbolField);
+
+            panel1.revalidate();
+            panel1.repaint();
+        });
+        
+        panel1.add(new JLabel("Race length: "));
+        panel1.add(raceLengthField);
+        panel1.add(new JLabel("Horses: "));
+        panel1.add(addHorseBtn);
+
+        JPanel panel2 = new JPanel(new GridLayout(2, 1));
+        JButton startRaceButton = new JButton("Start race");
+        JButton cancelButton = new JButton("Cancel");
+
+        panel2.add(startRaceButton);
+        panel2.add(cancelButton);
+
+        menuFrame.add(panel1, BorderLayout.CENTER);
+        menuFrame.add(panel2, BorderLayout.SOUTH);
+        menuFrame.setVisible(true);
     }
 
     public void addHorse(HorseV2 horseToAdd) {
