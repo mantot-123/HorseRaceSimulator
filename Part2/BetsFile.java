c01d0eac9da2e0201b7ab8f0026110b9027506c9
emman123 <67055497+emman123667@users.noreply.github.com>
2025-04-16 21:21:51 +0100

    Commit: Added 3 new files: CurrentBets.csv, PastBets.csv and BetsFile.java

---
+++ Part2/BetsFile.java
@@ -0,0 +1,6 @@
+import java.util.ArrayList;
+
+public class BetsFile {
+    private static ArrayList<Bet> currentBets = new ArrayList<Bet>();
+    private static ArrayList<Bet> pastBets = new ArrayList<Bet>();
+}
