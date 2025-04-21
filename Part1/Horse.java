12b482fbe76a1ef8aacca5adea1dda4f124be196
emman123 <67055497+emman123667@users.noreply.github.com>
2025-04-21 20:42:17 +0100

    Additional commit for Part 1: Organised TestHorse into different methods + adjusted condition for confidence rating decrease in the Horse class

--- Part1/Horse.java
+++ Part1/Horse.java
@@ -32,8 +32,8 @@ public class Horse
         this.isFallen = true;
 
         // decrease confidence rating by 0.1
-        // only do this as long as the confidence rating is above 0.1
-        if (this.confidenceRating > 0.1) {
+        // only do this as long as the confidence rating is above 0.11
+        if (this.confidenceRating > 0.11) {
             setConfidence(this.confidenceRating - 0.1);
         }
     }
--- Part1/TestHorse.java
+++ Part1/TestHorse.java
@@ -1,37 +1,55 @@
 public class TestHorse {
     public static void testHorse1() {
-        Horse horse1 = new Horse('H', "Pippi Longstocking", 0.6);
-        System.out.println("Horse name: " + horse1.getName()); // Shows name of the horse
-        System.out.println("Horse symbol: " + horse1.getSymbol()); // Shows symbol of the horse
-        System.out.println("Horse confidence: " + horse1.getConfidence()); // Shows confidence of the horse
-        System.out.println("Distance travelled: " + horse1.getDistanceTravelled()); // Shows distance of the horse
-        System.out.println("Is the horse fallen: " + horse1.hasFallen()); // Shows the "fallen" flag of the horse
+        Horse horse = new Horse('H', "Pippi Longstocking", 0.6);
+        System.out.println("Horse name: " + horse.getName()); // Shows name of the horse
+        System.out.println("Horse symbol: " + horse.getSymbol()); // Shows symbol of the horse
+        System.out.println("Horse confidence: " + horse.getConfidence()); // Shows confidence of the horse
+        System.out.println("Distance travelled: " + horse.getDistanceTravelled()); // Shows distance of the horse
+        System.out.println("Is the horse fallen: " + horse.hasFallen()); // Shows the "fallen" flag of the horse
         // Shows distance of the horse
-        System.out.println("Distance travelled before moving: " + horse1.getDistanceTravelled()); 
+        System.out.println("Distance travelled before moving: " + horse.getDistanceTravelled()); 
+    }
 
+    public static void testHorse2() {
+        Horse horse = new Horse('H', "Pippi Longstocking", 0.6);
         // Updates the distance travelled by 2 steps
-        horse1.moveForward();
-        horse1.moveForward();
-        System.out.println("Distance travelled after moving by 2 steps: " + horse1.getDistanceTravelled());
-        
+        horse.moveForward();
+        horse.moveForward();
+        System.out.println("Distance travelled after moving by 2 steps: " + horse.getDistanceTravelled());   
+    }
+
+    public static void testHorse3() {
+        Horse horse = new Horse('H', "Pippi Longstocking", 0.6);
         // Updates the "fallen" flag of the horse
-        horse1.fall();
-        System.out.println("Is the horse fallen: " + horse1.hasFallen());
-        System.out.println("Confidence level after falling: " + horse1.getConfidence());
+        horse.fall();
+        System.out.println("Is the horse fallen: " + horse.hasFallen());
+        System.out.println("Confidence level after falling: " + horse.getConfidence());
+    }
 
+    public static void testHorse4() {
+        Horse horse = new Horse('H', "Pippi Longstocking", 0.6);
+        horse.moveForward();
+        horse.moveForward();
         // Resets horse distance 
-        horse1.goBackToStart();
-        System.out.println("DIstance travelled after going back to starting point: " + horse1.getDistanceTravelled());
+        horse.goBackToStart();
+        System.out.println("DIstance travelled after going back to starting point: " + horse.getDistanceTravelled());
+    }
 
+    public static void testHorse5() {
+        Horse horse = new Horse('H', "Pippi Longstocking", 0.6);
         // Updates the confidence of the horse (success)
-        horse1.setConfidence(0.9);
-        System.out.println("Horse confidence after updating: " + horse1.getConfidence()); // Shows confidence of the horse
+        horse.setConfidence(0.9);
+        System.out.println("Horse confidence after updating: " + horse.getConfidence()); // Shows confidence of the horse
         // Updates the confidence of the horse (fail)
-        horse1.setConfidence(1.1);
-        System.out.println("Horse confidence after updating: " + horse1.getConfidence()); // Shows confidence of the horse
+        horse.setConfidence(1.1);
+        System.out.println("Horse confidence after updating: " + horse.getConfidence()); // Shows confidence of the horse
+    }
 
+
+    public static void testHorse6() {
+        Horse horse = new Horse('H', "Pippi Longstocking", 0.6);
         // Set horse symbol
-        horse1.setSymbol('X');
-        System.out.println("Horse symbol after updating: " + horse1.getSymbol()); // Shows symbol of the horse
+        horse.setSymbol('X');
+        System.out.println("Horse symbol after updating: " + horse.getSymbol()); // Shows symbol of the horse
     }
 }
