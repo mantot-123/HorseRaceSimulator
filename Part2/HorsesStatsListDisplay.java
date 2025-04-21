fffee4ac2fe26d9d9dab0afdf41af6685eeae88a
emman123 <67055497+emman123667@users.noreply.github.com>
2025-04-21 19:41:08 +0100

    Commit: Win count + number of games played added for each horse

---
+++ Part2/HorsesStatsListDisplay.java
@@ -0,0 +1,5 @@
+import javax.swing.*;
+
+public class HorsesStatsListDisplay {
+    JFrame frame = new JFrame("Horse statistics");
+}
--- Part2/SpecialHorses.java
+++ Part2/SpecialHorses.java
@@ -2,7 +2,7 @@ public class SpecialHorses {
     // Class to store special purpose horses.
     // These can't be used for racing, they're intended to be used as placeholder values for storing race data
 
-    static final HorseV2 UNKNOWN_HORSE = new HorseV2("UNKNOWNHORSE", '-', "Unknown horse", 0.0);
-    static final HorseV2 NO_WINNER = new HorseV2("NOWINNER", '-', "Draw (No winner, all horses fallen)", 0.0);
+    static final HorseV2 UNKNOWN_HORSE = new HorseV2("UNKNOWNHORSE", '-', "Unknown horse", 0.0, 0, 0);
+    static final HorseV2 NO_WINNER = new HorseV2("NOWINNER", '-', "Draw (No winner, all horses fallen)", 0.0, 0, 0);
     
 }
